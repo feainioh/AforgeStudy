a38a5980d7ea2f4760b4c7b7dc0016fc7c656acb
林秋照 <lqzxhf@163.com>
2019-12-15 13:57:18 +0800

    1打开多个相机；

--- AforgeWPF/Capture.xaml.cs
+++ AforgeWPF/Capture.xaml.cs
@@ -1,5 +1,7 @@
-﻿using AForge.Video;
+﻿using AForge.Controls;
+using AForge.Video;
 using AForge.Video.DirectShow;
+using AForge.Video.FFMPEG;
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
@@ -26,6 +28,12 @@ namespace AforgeWPF
     {
         FilterInfoCollection cameras = null;
         VideoCaptureDevice cam = null;
+        private FilterInfoCollection videoDevices;  //摄像头设备
+        private VideoCaptureDevice videoSource;     //视频的来源选择
+        private VideoSourcePlayer videoSourcePlayer;    //AForge控制控件
+        private VideoFileWriter writer;     //写入到视频
+        private bool is_record_video = false;   //是否开始录像
+        System.Timers.Timer timer_count;
         public Capture()
         {
             InitializeComponent();
@@ -69,9 +77,9 @@ namespace AforgeWPF
                     cmb_DevicesName.SelectedIndex = 0;
                 }
             }
-            catch
+            catch(Exception ex)
             {
-                MessageBox.Show("Get Camera name error");
+                MessageBox.Show("Get Camera name error:"+ex.Message);
             }
         }
 
@@ -88,9 +96,9 @@ namespace AforgeWPF
                     btn_Capture.IsEnabled = true;
                 }
             }
-            catch
+            catch(Exception ex)
             {
-                MessageBox.Show("Open camera Error");
+                MessageBox.Show("Open camera Error:"+ ex.Message);
             }
         }
 
@@ -98,13 +106,43 @@ namespace AforgeWPF
         {
             try
             {
-                Bitmap bitmap = sourcePlayer.GetCurrentVideoFrame();
-                picture.Source =(BitmapImage) bitmap.Clone();
-                bitmap.Dispose();
+                //Bitmap bitmap = sourcePlayer.GetCurrentVideoFrame();
+                //IntPtr ip = bitmap.GetHbitmap();//从GDI+ Bitmap创建GDI位图对象
+
+                //BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(ip, IntPtr.Zero, Int32Rect.Empty,
+                //BitmapSizeOptions.FromEmptyOptions());
+                //picture.Source =bitmapSource;
+                //bitmap.Dispose();
+                int width = 640;    //录制视频的宽度
+                int height = 480;   //录制视频的高度
+                int fps = 9;
+
+                //创建一个视频文件
+                String video_format = this.cmb_DevicesName.Text.Trim(); //获取选中的视频编码
+                if (this.videoSource.IsRunning && this.videoSourcePlayer.IsRunning)
+                {
+                    if (-1 != video_format.IndexOf("MPEG"))
+                    {
+                        writer.Open("test.avi", width, height, fps, VideoCodec.MPEG4);
+                    }
+                    else if (-1 != video_format.IndexOf("WMV"))
+                    {
+                        writer.Open("test.wmv", width, height, fps, VideoCodec.WMV1);
+                    }
+                    else
+                    {
+                        writer.Open("test.mkv", width, height, fps, VideoCodec.Default);
+                    }
+                }
+                else
+                    MessageBox.Show("没有视频源输入，无法录制视频。", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                timer_count.Enabled = true;//是否执行System.Timers.Timer.Elapsed事件；
+                this.is_record_video = true;
             }
-            catch
+            catch(Exception ex)
             {
-                MessageBox.Show("Capture Error");
+                MessageBox.Show("Capture Error:"+ex.Message);
             }
         }
 
---
+++ AforgeWPF/ViedoCaptureWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿/**************************************************************************
+*   
+*   =================================
+*   CLR版本    ：4.0.30319.42000
+*   命名空间    ：AforgeWPF
+*   文件名称    ：ViedoCaptureWindow.cs
+*   =================================
+*   创 建 者    ：LQZ
+*   创建日期    ：2019-12-6 14:15:19 
+*   功能描述    ：
+*   =================================
+*   修改者    ：
+*   修改日期    ：
+*   修改内容    ：
+*   =================================
+*  
+***************************************************************************/
+using AForge.Controls;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AforgeWPF
+{
+    /// <summary>
+    /// ViedoCaptureWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ViedoCaptureWindow : Window
+    {
+        VideoCaptureDevice cam = null;
+        private FilterInfoCollection videoDevices;  //摄像头设备
+        private VideoCaptureDevice videoSource;     //视频的来源选择
+        private VideoSourcePlayer videoSourcePlayer;    //AForge控制控件
+        private VideoFileWriter writer;     //写入到视频
+        VideoFileWriter writer1;
+        VideoFileWriter writer2;
+        private bool is_record_video = false;   //是否开始录像
+        List<VideoCaptureDevice> list_Cam = new List<VideoCaptureDevice>();
+        private ScreenCaptureStream videoStreamer;
+
+        public ViedoCaptureWindow()
+        {
+            InitializeComponent();
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count > 0)
+            {
+                for (int i = 0; i < videoDevices.Count; i++)
+                {
+                    VideoCaptureDevice cap = new VideoCaptureDevice(videoDevices[i].MonikerString);
+                    cap.VideoResolution = cap.VideoCapabilities[0];
+
+                    list_Cam.Add(cap);
+                }
+            }
+        }
+
+        private void Btn_Open_Click(object sender, RoutedEventArgs e)
+        {
+            if (list_Cam.Count >= 3)
+            {
+                sourcePlayer_PC.VideoSource = new AsyncVideoSource(list_Cam[0]);
+                sourcePlayer_Usb.VideoSource = new AsyncVideoSource(list_Cam[2]);
+                sourcePlayer_Link.VideoSource = new AsyncVideoSource(list_Cam[1]);
+                lb_PC.Content = videoDevices[0].MonikerString;
+                lb_usb.Content = videoDevices[2].MonikerString;
+                lb_link.Content = videoDevices[1].MonikerString;
+                sourcePlayer_PC.Start();
+                sourcePlayer_Usb.Start();
+                sourcePlayer_Link.Start();
+            }
+        }
+
+        private void Btn_Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (sourcePlayer_PC.IsRunning && sourcePlayer_Link.IsRunning && sourcePlayer_Usb.IsRunning)
+            {
+                writer = new VideoFileWriter();
+                //打开写入流                
+                writer.Open(@"D:\EmguCV\aaa.avi", list_Cam[0].VideoResolution.FrameSize.Width, list_Cam[0].VideoResolution.FrameSize.Height, 30, (VideoCodec)3, 3000000);
+                //开始录制
+                sourcePlayer_PC.NewFrame += SourcePlayer_PC_NewFrame;
+                writer1 = new VideoFileWriter();
+                //打开写入流
+                writer1.Open(@"D:\EmguCV\bbb.avi", list_Cam[2].VideoResolution.FrameSize.Width, list_Cam[2].VideoResolution.FrameSize.Height, 30, (VideoCodec)3, 3000000);
+                sourcePlayer_Usb.NewFrame += SourcePlayer_Usb_NewFrame;
+                writer2 = new VideoFileWriter();
+                //打开写入流
+                writer2.Open(@"D:\EmguCV\ccc.avi", list_Cam[1].VideoResolution.FrameSize.Width, list_Cam[1].VideoResolution.FrameSize.Height, 30, (VideoCodec)3, 3000000);
+                sourcePlayer_Link.NewFrame += SourcePlayer_Link_NewFrame;
+            }
+        }
+
+        private void SourcePlayer_Link_NewFrame(object sender, ref Bitmap image)
+        {
+            writer2.WriteVideoFrame(image);
+            Thread.Sleep(10);
+        }
+
+        private void SourcePlayer_Usb_NewFrame(object sender, ref Bitmap image)
+        {
+            writer1.WriteVideoFrame(image);
+            Thread.Sleep(10);
+        }
+
+        private void SourcePlayer_PC_NewFrame(object sender, ref System.Drawing.Bitmap image)
+        {
+            writer.WriteVideoFrame(image);
+            Thread.Sleep(10);
+        }
+
+        private void Btn_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            sourcePlayer_PC.Stop();
+            sourcePlayer_Usb.Stop();
+            sourcePlayer_Link.Stop();
+        }
+    }
+}
